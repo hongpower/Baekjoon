b0b7c81d8ffd9b7b56bb82b8574d75508fd8a1cc
지수 <jisuhongeism@gmail.com>
2022-01-28 22:21:01 +0900

    math4

---
+++ Mathematics_01/2775.py
@@ -0,0 +1,26 @@
+## 부녀회장이 될테야
+# k : 층 n: 호
+# 0층부터 1호까지. 0층의 i호에는 i명이 산다
+import sys
+t = int(sys.stdin.readline())
+
+# 한 층 거주자 (Resident Num)
+for repeat in range(t):
+    k = int(sys.stdin.readline())  # 층수
+    n = int(sys.stdin.readline())  # 호수
+    ResN = [i for i in range(1,n+1)]
+    floor = 0
+
+    while True:
+        floor += 1
+        for j in range(1,n):
+            ResN[j] = ResN[j-1] + ResN[j]
+        if floor == k:
+            break
+
+    print(ResN[n-1])
+
+
+
+
+
